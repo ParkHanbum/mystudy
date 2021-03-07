efbdae50d8209c3b0b4910def233726d748a8d57
Hanbum Park <kese111@gmail.com>
2021-03-08 01:03:42 +0900

    temp 3

--- CFI.cpp
+++ CFI.cpp
@@ -22,95 +22,79 @@ llvm::BasicBlock *createBasicBlock(
 
 void CFI::handleInst(Instruction *inst)
 {
-  if (isa<CallInst>(inst) || isa<InvokeInst>(inst))
-  {
-    auto *call = dyn_cast<CallInst>(inst);
-    
-    if (!call->isIndirectCall())
-      return;
-    
-    debugInst(call);
-    auto *fnType = call->getFunctionType();
-
-    if (fnType)
-      fnType->print(errs(), true);
-
-
-    Value *v = call->getCalledValue();
-    Value *sv = v->stripPointerCasts();
-
-    LLVMContext &context = inst->getFunction()->getContext();
-    IRBuilder<> Builder(inst);
-    Type *ty = Type::getInt8PtrTy(context);
-    Value *CastedCallee = Builder.CreateBitCast(sv, ty);
-    Function *func = Intrinsic::getDeclaration(inst->getModule(), Intrinsic::type_test);
-
-    Metadata *MD = MDString::get(context, "fnPtr");
-    Value *TypeID = MetadataAsValue::get(context, MD);
-    Instruction *TypeTest = Builder.CreateCall(func, {CastedCallee, TypeID});
-
-    BasicBlock *TypeTestBB = TypeTest->getParent();
-    BasicBlock *ContBB = TypeTestBB->splitBasicBlock(TypeTest, "cont");
-    BasicBlock *TrapBB = TypeTestBB->splitBasicBlock(TypeTest, "trap");
-    /*
-    Instruction *CondBR = Builder.CreateCondBr(TypeTest, ContBB, TrapBB);
-    Builder.SetInsertPoint(TrapBB);
-    CallInst *TrapCall = Builder.CreateCall(Intrinsic::getDeclaration(inst->getModule(), Intrinsic::trap));
-    TrapCall->setDoesNotReturn();
-    TrapCall->setDoesNotThrow();
-    Builder.CreateUnreachable();
-    */
-  }
-  else if (isa<StoreInst>(inst))
-  {
-    errs() << "store \n";
-    debugInst(inst);
-
-   if (inst->getNumOperands() != 2)
-     return;
-
-   Value *op1 = inst->getOperand(0);
-   Value *op2 = inst->getOperand(1);
-   if (!op1->getType()->isPointerTy() || !op2->getType()->isPointerTy())
-     return;
-
-   if (!isa<Constant>(op1))
-     return;
-
-   Function *pFunc = NULL;
-   // extract function pointer from op1 if it is ConstantExpr.
-   if (isa<ConstantExpr>(op1)) {
-     errs() << "constantExpr";
-     auto *expr = dyn_cast<ConstantExpr>(op1);
-     if (isa<Function>(expr->getOperand(0)))
-       pFunc = dyn_cast<Function>(expr->getOperand(0));
-   } else if (isa<Function>(op1)) {
-     errs() << "function";
-     pFunc = dyn_cast<Function>(op1);
-   }
-
-   // handle function pointer
-   if (pFunc)
-     errs() << "handle Function pointer\n";
-  }
 }
 
 void CFI::handleBB(BasicBlock *bb)
 {
-   for (BasicBlock::iterator iter = bb->begin(), E = bb->end(); iter != E; ++iter)
-   {
-     Instruction *inst = &*iter;
-     handleInst(inst);
-   }
 }
 
 void CFI::handleFunction(Function *func)
 {
-   for (Function::iterator BB = func->begin(), E = func->end(); BB != E; ++BB)
-   {
-     BasicBlock *bb = &*BB;
-     handleBB(bb);
-   }
+  BasicBlock *bb;
+
+  for (Function::iterator BB = func->begin(), E = func->end(); BB != E; ++BB)
+  {
+    if (bb == &*BB)
+      continue;
+    
+    bb = &*BB;
+    Instruction *inst;
+
+    for (BasicBlock::iterator iter = bb->begin(), E = bb->end(); iter != E; ++iter)
+    {
+      if (inst == &*iter)
+        continue;
+      inst = &*iter;
+      if (isa<CallInst>(inst) || isa<InvokeInst>(inst))
+      {
+        auto *call = dyn_cast<CallInst>(inst);
+
+        if (!call->isIndirectCall())
+          return;
+
+        auto *fnType = call->getFunctionType();
+
+        Value *v = call->getCalledValue();
+        Value *sv = v->stripPointerCasts();
+
+        LLVMContext &context = inst->getFunction()->getContext();
+        IRBuilder<> Builder(inst);
+        Type *ty = Type::getInt8PtrTy(context);
+        Value *CastedCallee = Builder.CreateBitCast(sv, ty);
+        Function *func = Intrinsic::getDeclaration(inst->getModule(), Intrinsic::type_test);
+
+        Metadata *MD = MDString::get(context, "fnPtr");
+        Value *TypeID = MetadataAsValue::get(context, MD);
+        Instruction *TypeTest = Builder.CreateCall(func, {CastedCallee, TypeID});
+        BasicBlock *TypeTestBB = TypeTest->getParent();
+
+        BasicBlock *ContBB = TypeTestBB->splitBasicBlock(TypeTest, "cont");
+        BasicBlock *TrapBB = createBasicBlock(context, "trap"); 
+        TrapBB->insertInto(inst->getFunction(), ContBB);
+
+        // remove branch instruction which created by splitBasicBlock calling
+        errs() << "DEBUGGGGGG";
+        Instruction *temp = dyn_cast<Instruction>(CastedCallee); 
+        Instruction *remove = temp->getNextNode();
+        remove->removeFromParent();
+
+        // move @llvm.type.test to right position
+        debugInst(TypeTest);
+        TypeTest->removeFromParent();
+        TypeTest->insertAfter(cast<Instruction>(CastedCallee));
+
+        Builder.SetInsertPoint(TypeTestBB);
+        Instruction *CondBR = Builder.CreateCondBr(TypeTest, ContBB, TrapBB);
+        Builder.SetInsertPoint(TrapBB);
+        CallInst *TrapCall = Builder.CreateCall(Intrinsic::getDeclaration(inst->getModule(), Intrinsic::trap));
+        TrapCall->setDoesNotReturn();
+        TrapCall->setDoesNotThrow();
+        Builder.CreateUnreachable();
+
+        return;
+      }
+    }
+  }
 }
 
 //-----------------------------------------------------------------------------
@@ -125,7 +109,6 @@ bool CFI::runOnModule(Module &M) {
     for (auto &BB : Func) {
       errs() << "\n======================== [bb] =========================\n";
       BB.print(errs(), false);
-      errs() << "\n======================== [bb] =========================\n";
 
       for (auto &Ins : BB) {
         debugInst(&Ins);
