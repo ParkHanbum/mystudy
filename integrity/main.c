30c5fda4d1e2a71a791e3429ce48f9c7365e3d24
Hanbum Park <kese111@gmail.com>
2022-11-15 02:15:14 +0900

    Misc : misc code refactoring

--- integrity/main.c
+++ integrity/main.c
@@ -9,76 +9,76 @@
 #include "aes.h"
 #include "integrity.h"
 
-void print_hexstr(const unsigned char *data, int len)
-{
-    int i;
+void print_hexstr(const unsigned char *data, int len) {
+  int i;
 
-    for (i = 0; i < len; i++) {
-        printf("%c%c ", data[i*2], data[i*2+1]);
-    }
+  for (i = 0; i < len; i++) {
+    printf("%c%c ", data[i * 2], data[i * 2 + 1]);
+  }
 
-    printf("\n");
+  printf("\n");
 }
 
-void print_hex(const unsigned char *data, int len)
-{
-    int i;
+void print_hex(const unsigned char *data, int len) {
+  int i;
 
-    for (i = 0; i < len; i++) {
-        printf("%02x ", data[i]);
-    }
+  for (i = 0; i < len; i++) {
+    printf("%02x ", data[i]);
+  }
 
-    printf("\n");
+  printf("\n");
 }
 
-void testAES(char *argv)
-{
-    unsigned char iv[AES_BLOCK_SIZE] = { 0, };
-    unsigned char *_enc, *_dec, *in;
-    int in_length;
+void testAES(char *argv) {
+  unsigned char iv[AES_BLOCK_SIZE] = {
+      0,
+  };
+  unsigned char *_enc, *_dec, *in;
+  int in_length;
 
-    in = (unsigned char *) argv;
-    in_length = strlen(in);
-    in_length = ((in_length / AES_BLOCK_SIZE) * AES_BLOCK_SIZE) + AES_BLOCK_SIZE;
+  in = (unsigned char *)argv;
+  in_length = strlen(in);
+  in_length = ((in_length / AES_BLOCK_SIZE) * AES_BLOCK_SIZE) + AES_BLOCK_SIZE;
 
-    _enc = (unsigned char *)malloc(in_length);
-    _dec = (unsigned char *)malloc(in_length);
+  _enc = (unsigned char *)malloc(in_length);
+  _dec = (unsigned char *)malloc(in_length);
 
-    print_hex(in, in_length);
-    enc(in, _enc, in_length);
-    print_hex(_enc, in_length);
+  print_hex(in, in_length);
+  enc(in, _enc, in_length);
+  print_hex(_enc, in_length);
 
-    dec(in, _dec, in_length);
-    print_hex(_dec, in_length);
+  dec(in, _dec, in_length);
+  print_hex(_dec, in_length);
 }
 
-char const hex_chars[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+char const hex_chars[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
-char *convert_chararr_to_hexarr(char *arr, int leng, unsigned char *out)
-{
-    int i, j;
-    for (i = 0; i < leng; i++) {
-      for (j = 0; j < 16; j++) {
-        if (arr[i] == hex_chars[j]) {
-          if (i % 2 == 0) {
-            out[i/2] |= (j << 4);
-          }
-          else {
-            out[i/2] |= (j);
-          }
-          break;
+char *convert_chararr_to_hexarr(char *arr, int leng, unsigned char *out) {
+  int i, j;
+  for (i = 0; i < leng; i++) {
+    for (j = 0; j < 16; j++) {
+      if (arr[i] == hex_chars[j]) {
+        if (i % 2 == 0) {
+          out[i / 2] |= (j << 4);
+        } else {
+          out[i / 2] |= (j);
         }
+        break;
       }
     }
-    return out;
+  }
+  return out;
 }
 
 char test[] = "Hell World!";
 char result[] = "d41dfe6726e9b099548518086d0ce9d39b8691317792d1eafd4aa9755462e3f5";
-void testSHA256()
-{
-  unsigned char digest[SHA256_DIGEST_LENGTH] = {0,};
-  unsigned char cmp[SHA256_DIGEST_LENGTH] = {0,};
+void testSHA256() {
+  unsigned char digest[SHA256_DIGEST_LENGTH] = {
+      0,
+  };
+  unsigned char cmp[SHA256_DIGEST_LENGTH] = {
+      0,
+  };
 
   SHA256((unsigned char *)test, strlen(test), (unsigned char *)&digest);
   printf("TEST SHA256 \n");
@@ -88,20 +88,23 @@ void testSHA256()
     printf("SHA256 AVAILIABLE\n");
 }
 
-void testINTEGRITY1()
-{
-    int i;
-    for (i = 0; i < DEFINED_FILE_COUNT; i++) {
-        struct hash_pair curr = hashes[i];
-        print_hex(curr.namehash, SHA256_DIGEST_LENGTH);
-        print_hex(curr.datahash, SHA256_DIGEST_LENGTH);
-    }
+void testINTEGRITY1() {
+  int i;
+  for (i = 0; i < DEFINED_FILE_COUNT; i++) {
+    struct hash_pair curr = hashes[i];
+    printf("%d] ", i);
+    print_hex(curr.namehash, SHA256_DIGEST_LENGTH);
+    print_hex(curr.datahash, SHA256_DIGEST_LENGTH);
+  }
 }
 
-void testINTEGRITY2()
-{
-  unsigned char namehash[SHA256_DIGEST_LENGTH] = {0,};
-  unsigned char datahash[SHA256_DIGEST_LENGTH] = {0,};
+void testINTEGRITY2() {
+  unsigned char namehash[SHA256_DIGEST_LENGTH] = {
+      0,
+  };
+  unsigned char datahash[SHA256_DIGEST_LENGTH] = {
+      0,
+  };
   unsigned char path[PATH_MAX];
   int i;
   char *buf;
@@ -132,9 +135,11 @@ void testINTEGRITY2()
       for (i = 0; i < DEFINED_FILE_COUNT; i++) {
         struct hash_pair curr = hashes[i];
         if (memcmp(namehash, curr.namehash, SHA256_DIGEST_LENGTH) == 0) {
-          printf("hash found! : ");
+          printf("[%d] hash found! : ", i);
+          print_hex(namehash, SHA256_DIGEST_LENGTH);
           if (memcmp(datahash, curr.datahash, SHA256_DIGEST_LENGTH) == 0) {
-            printf("correct!\n");
+            printf("correct! : ");
+            print_hex(datahash, SHA_DIGEST_LENGTH);
           } else {
             printf("hash tampering : ");
             print_hex(datahash, SHA256_DIGEST_LENGTH);
@@ -147,24 +152,27 @@ void testINTEGRITY2()
   closedir(dp);
 }
 
-void testINTEGRITY3()
-{
-    int i;
+void testINTEGRITY3() {
+  int i;
 
-    for (i = 0; i < BUCKET_COUNT; i++) {
-        int hash_idx = ind[i].hash_idx;
-        int count = ind[i].count;
-        if (count == 0) continue;
-        printf("%d : %d %d\n", i, hash_idx, count);
-        print_hex(hashes[hash_idx].namehash, SHA256_DIGEST_LENGTH);
-        print_hex(hashes[hash_idx].datahash, SHA256_DIGEST_LENGTH);
-    }
+  for (i = 0; i < BUCKET_COUNT; i++) {
+    int hash_idx = ind[i].hash_idx;
+    int count = ind[i].count;
+    if (count == 0)
+      continue;
+    printf("%d : %d %d\n", i, hash_idx, count);
+    print_hex(hashes[hash_idx].namehash, SHA256_DIGEST_LENGTH);
+    print_hex(hashes[hash_idx].datahash, SHA256_DIGEST_LENGTH);
+  }
 }
 
-void testINTEGRITY4()
-{
-  uint8_t namehash[SHA256_DIGEST_LENGTH] = {0,};
-  uint8_t datahash[SHA256_DIGEST_LENGTH] = {0,};
+void testINTEGRITY4() {
+  uint8_t namehash[SHA256_DIGEST_LENGTH] = {
+      0,
+  };
+  uint8_t datahash[SHA256_DIGEST_LENGTH] = {
+      0,
+  };
   unsigned char path[PATH_MAX];
   int i;
   char *buf;
@@ -227,8 +235,7 @@ void testINTEGRITY4()
   closedir(dp);
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   testAES(argv[1]);
   testSHA256();
   testINTEGRITY1();
